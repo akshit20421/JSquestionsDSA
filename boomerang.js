abeccc6ff2919bcaf2692b9d9e567d263636c650
akshit20421 <103023426+akshit20421@users.noreply.github.com>
2023-08-23 08:16:49 +0000

    boomerang

---
+++ boomerang.js
@@ -0,0 +1,11 @@
+var isBoomerang = function(points) {
+    // x1 (y2 - y3) + x2 (y3 - y1) + x3 (y1 - y2) = 0
+    let [[x1, y1], [x2, y2], [x3, y3]] = points
+    return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) == 0 ? false : true
+};
+
+
+
+
+var cll = isBoomerang([1,1],[2,3],[3,2]);
+console.log(cll)
