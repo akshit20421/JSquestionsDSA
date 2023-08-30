3b88c4b1246640ece74aa45bb27ead5a1f986fe8
akshit20421 <103023426+akshit20421@users.noreply.github.com>
2023-08-30 10:33:19 +0000

    updated ugly number

--- uglyNumber.js
+++ uglyNumber.js
@@ -1,22 +1,22 @@
 var isUgly = function(n) {
-let arr = [2,3,5];
-let point=0;
-  if(n<0) return false
-while(point<=arr.length-1){
-  if(n%arr[point]==0){
-    let div = n/arr[point]
-  if(div==arr[point]){
-    return true
-    break
-  }
-    else{
-      point++;
+    if (n <= 0) {
+        return false;
     }
-  }
-}
-return false
 
+    let factors = [2, 3, 5];
+    let point = 0;
+
+    while (point < factors.length) {
+        if (n % factors[point] === 0) {
+            n /= factors[point];
+        } else {
+            point++;
+        }
+    }
+
+    return n === 1;
 };
+
 
 var cll = isUgly(6);
 console.log(cll)
