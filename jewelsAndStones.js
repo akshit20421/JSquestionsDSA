919f12c29897e5c3c95fe109153602a6f0f1265b
akshit20421 <103023426+akshit20421@users.noreply.github.com>
2023-09-05 18:50:19 +0000

    Jewels and Stones

---
+++ jewelsAndStones.js
@@ -0,0 +1,11 @@
+var numJewelsInStones = function(jewels, stones) {
+    var count=0
+    for(let i=0;i<jewels.length;i++){
+        for(let j=0;j<stones.length;j++){
+            if(jewels[i]==stones[j]){
+                count++
+            }
+        }
+    }
+    return count
+};
