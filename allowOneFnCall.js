82ccb44f36042d373913ab8504b95a0fa4c9b234
akshit20421 <103023426+akshit20421@users.noreply.github.com>
2023-09-16 11:55:19 +0000

    Allow one Function Call

---
+++ allowOneFnCall.js
@@ -0,0 +1,13 @@
+var once = function(fn) {
+  var called=false;
+    return function(...args){
+      if(!called){
+        called=true
+        return fn(...args);
+      }
+    }
+};
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn);
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns undefined without calling fn
